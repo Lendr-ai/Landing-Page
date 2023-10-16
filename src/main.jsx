5333e1b5eb195a8f6ea9689fb4e0ded849773799
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-10-16 23:54:40 +1100

    added fix for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/<Landing-Page>/"}>
+    <BrowserRouter basename={"/Landing-Page/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
